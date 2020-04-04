7f53976b5d228658627fa48cff439cc9f01dfa87
Vansh1067 <50477887+Vansh1067@users.noreply.github.com>
2020-04-04 22:35:40 +0530

    new ui

--- components/card.js
+++ components/card.js
@@ -1,21 +1,23 @@
 import React from 'react';
-import {View ,Text,StyleSheet} from 'react-native';
+import { View, StyleSheet } from 'react-native';
 
-const Card=(props)=>{
-return <View style={{...style.card, ...props.style}}>{props.children}</View>
-}
- const style=StyleSheet.create({
-     card:{
-        shadowColor:'black',
-        shadowOffset:{
-            width:0,
-            height:2
-        },
-        shadowRadius:6,
-        shadowOpacity:0.3,
-        elevation:8,
-        backgroundColor:'white',
-        padding:20
-     }
- })
-export default Card
+const Card = props => {
+  return (
+    <View style={{ ...styles.card, ...props.style }}>{props.children}</View>
+  );
+};
+
+const styles = StyleSheet.create({
+  card: {
+    shadowColor: 'black',
+    shadowOffset: { width: 0, height: 2 },
+    shadowRadius: 6,
+    shadowOpacity: 0.26,
+    elevation: 8,
+    backgroundColor: 'white',
+    padding: 20,
+    borderRadius: 10
+  }
+});
+
+export default Card;
--- components/header.js
+++ components/header.js
@@ -1,28 +1,29 @@
 import React from 'react';
-import {View ,Text,StyleSheet} from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
 
-const header=(props)=>{
-    return (
-        <View style={style.header}>
-            <Text style={style.headerTitle}>{props.title}</Text>
-        </View>
-    )
-}
+import Colors from '../constants/colors';
 
-const style=StyleSheet.create({
-    header:{
-        width:'100%',
-        height:90,
-        paddingTop:40,
-        backgroundColor:'#f7287b',
-        alignItems:'center',
-        justifyContent:'center',
-    },
-    headerTitle:{
-        color:'white',
-        fontSize:18
+const Header = props => {
+  return (
+    <View style={styles.header}>
+      <Text style={styles.headerTitle}>{props.title}</Text>
+    </View>
+  );
+};
 
-    }
-})
+const styles = StyleSheet.create({
+  header: {
+    width: '100%',
+    height: 90,
+    paddingTop: 36,
+    backgroundColor: Colors.primary,
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  headerTitle: {
+    color: 'black',
+    fontSize: 18
+  }
+});
 
-export default header
+export default Header;
